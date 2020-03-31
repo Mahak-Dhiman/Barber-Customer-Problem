32968cdd4c56acd72b463619bd26e86c80503b6c
Mahak-Dhiman <58025022+Mahak-Dhiman@users.noreply.github.com>
2020-03-31 13:04:52 +0530

    Update os_project.c

--- os_project.c
+++ os_project.c
@@ -1,4 +1,4 @@
-/* A barbershop consists of a waiting room with n chairs and a barber room with one barber chair. 
+/* Ques 13. A barbershop consists of a waiting room with n chairs and a barber room with one barber chair. 
 If there are no customers to be served, the barber goes to sleep. 
 If a customer enters the barbershop and all chairs are occupied, then the customer leaves the shop.
 If the barber is busy but chairs are available, then the customer sits in one of the free chairs. 
